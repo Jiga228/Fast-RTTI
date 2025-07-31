cb1b040b6bc6724419280f6f6ac8dcf57fa249aa
Данил <119668441+Jiga228@users.noreply.github.com>
2025-07-31 19:34:02 +0700

    Fix bug

--- RTTI/RTTI_Meta.h
+++ RTTI/RTTI_Meta.h
@@ -4,7 +4,7 @@
 #define GENERATE_META(Class) \
 class Class; \
 template<> struct Meta <Class> { \
-static constexpr Classes Type = Classes::##Class; \
+static constexpr Classes Type = Classes::Class; \
 };
 
 // Predeclared struct for meta information
